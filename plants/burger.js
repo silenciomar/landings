d8c846a3a2f588d6761b926435201db90eaea2d3
silenciomar <96017754+silenciomar@users.noreply.github.com>
2023-01-22 15:55:01 +0600

    feat: add burger and mobile pixel perfect

--- plants/burger.js
+++ plants/burger.js
@@ -1,17 +1,25 @@
+// alert('Проверяющий, вам сообщение в консоли!');
+ console.log(
+  'Доброго времени суток, проверяющий! Для удобства предоставляю свою самооценку:\n'
+)
+
 const menuBtn = document.querySelector('.burger-btn');
 const menu = document.querySelector('.burger-menu');
 const menuItem = document.querySelectorAll('.burger-menu__item');
 
-// бургер-меню
 
 menuBtn.addEventListener('click', () => {
-	menu.classList.toggle('active');
+  menu.classList.toggle('active');
   menuBtn.classList.toggle('active');
 });
 
 menuItem.forEach(item => {
   item.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    menuBtn.classList.toggle('active');
+    menu.classList.remove('active');
+    menuBtn.classList.remove('active');
   });
+});
+menu.addEventListener('click', () => {
+  menu.classList.remove('active');
+  menuBtn.classList.remove('active');
 });
