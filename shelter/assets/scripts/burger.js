e903a5487aa9bc9e5c1d8b1a5083033b98ef4a8a
silenciomar <96017754+silenciomar@users.noreply.github.com>
2023-04-03 21:55:25 +0600

    feat: add mobile version

---
+++ shelter/assets/scripts/burger.js
@@ -0,0 +1,20 @@
+// const menuBtn = document.querySelector('.burger-btn');
+// const menu = document.querySelector('.burger-menu');
+// const menuItem = document.querySelectorAll('.burger-menu__item');
+
+
+// menuBtn.addEventListener('click', () => {
+//   menu.classList.toggle('active');
+//   menuBtn.classList.toggle('active');
+// });
+
+// menuItem.forEach(item => {
+//   item.addEventListener('click', () => {
+//     menu.classList.remove('active');
+//     menuBtn.classList.remove('active');
+//   });
+// });
+// menu.addEventListener('click', () => {
+//   menu.classList.remove('active');
+//   menuBtn.classList.remove('active');
+// });
